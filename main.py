cdc1df7e561941717764131668ca9e847baa49a7
Lucas Oliveri <oliverilucas@gmail.com>
2022-07-09 03:26:40 -0400

    Añadida la grilla

--- main.py
+++ main.py
@@ -1,43 +1,52 @@
 import pygame, sys
+import numpy as np
 from pygame.locals import *
 
 pygame.init()
-ventana = pygame.display.set_mode((600,300))
-pygame.display.set_caption("Tutorial Cuatro")
 
+#Parámetros de pantalla
+WINDOW_HEIGHT = 375 #alto
+WINDOW_WIDTH = 225 #ancho
+SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Tetris")
+grilla = []
+
+np.zeros((15,9))
 #Colores
-color_blanco = pygame.Color(255,255,255)
-color_negro = pygame.Color(0,0,0)
+WHITE = pygame.Color(255,255,255)
+BLACK = pygame.Color(0,0,0)
 
-pygame.draw.circle(ventana, color_negro, (15,15), 10, 5)
-
-imagen = pygame.image.load("img/main.png")
-pos_x = 200
-pos_y = 200
 velocidad = 50
 
+def drawGrid():
+    blockSize = 25 #Set the size of the grid block
+    for x in range(0, WINDOW_WIDTH, blockSize):
+        for y in range(0, WINDOW_HEIGHT, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+
+def setGrid():
+    for x in range(0, 9):
+        for y in range(0,15):
+            grilla[x][y] = 0
+
+
+
 while True:
-    ventana.fill(color_blanco)
-    ventana.blit(imagen, (pos_x, pos_y))
+    SCREEN.fill(BLACK)
+    drawGrid()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == K_LEFT:
-                pos_x -= velocidad
+                pass
             elif event.key ==K_RIGHT:
-                pos_x += velocidad
+                pass
             elif event.key ==K_UP:
-                pos_y -= velocidad
+                pass
             elif event.key ==K_DOWN:
-                pos_y += velocidad       
-
+                pass     
     pygame.display.update()
 
-def drawGrid():
-    blockSize = 20 #Set the size of the grid block
-    for x in range(0, WINDOW_WIDTH, blockSize):
-        for y in range(0, WINDOW_HEIGHT, blockSize):
-            rect = pygame.Rect(x, y, blockSize, blockSize)
-            pygame.draw.rect(SCREEN, WHITE, rect, 1)
